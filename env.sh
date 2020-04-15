4bdc91a2e92bd68ad3a5cd622e84caf7ee3d9008
tcaxle <tcac2@cam.ac.uk>
2020-04-15 17:20:46 +0100

    env.sh finished and .gitignore updates

--- env.sh
+++ env.sh
@@ -3,24 +3,24 @@
 echo "== BEGIN =="
 
 echo "== Upgrading pip =="
-pip install --user --upgrade pip
+pip3.8 install --user --upgrade pip
 
 echo "== Installing virtualenv =="
-pip install --user virtualenv
+pip3.8 install --user virtualenv
 
 echo "== Creating virtualenv 'env' =="
-virtualenv env
+python3.8 -m virtualenv env
 
 echo "== Activating virtualenv 'env' =="
 source env/bin/activate
 
 echo "== Installing requirements =="
-pip install django
-pip install psycopg2 # Needed for postgresql support
-pip install django-polymorphic # Needed for polymorphic model support
+pip3.8 install django
+pip3.8 install psycopg2 # Needed for postgresql support
+pip3.8 install django-polymorphic # Needed for polymorphic model support
                                # (allows querying of parent models to return children
 
-echo "== Creating symbolic links =="
+echo "== Creating symbolic link activate -> env/bin/activate =="
 ln -s env/bin/activate activate
 
 echo "** Please source activate (-> env/bin/activate) **"
